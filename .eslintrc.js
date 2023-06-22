b4a2377e81bfdbca6c32514ce1afcd99da55377a
linonetwo <linonetwo012@gmail.com>
2023-06-22 22:07:23 +0800

    refactor: use https://tiddly-gittly.github.io/Modern.TiddlyDev/

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const tsEslintConfig = require('./tsconfig.eslint.json');
 
 module.exports = {
@@ -15,12 +16,9 @@ module.exports = {
     react: {
       version: '17.0.2',
     },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
     'import/resolver': {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
       typescript: {
         alwaysTryTypes: true,
@@ -30,13 +28,11 @@ module.exports = {
           ['@', './src'],
           ['@services', './src/services'],
         ],
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
   rules: {
-    'import/no-unresolved': [2, { ignore: ['\\$:/'] }],
-    // 'no-use-before-define': ['error', { ignoreTypeReferences: true, functions: false }],
     'unicorn/prevent-abbreviations': [
       'error',
       {
@@ -52,11 +48,18 @@ module.exports = {
         },
       },
     ],
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'security-node/detect-unhandled-async-errors': 'off',
     'unicorn/prefer-node-protocol': 'off',
-    'unicorn/no-useless-undefined': 'off',
-    '@typescript-eslint/consistent-type-assertions': 'off',
     'unicorn/prefer-module': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    'security-node/detect-insecure-randomness': 'off',
     '@typescript-eslint/method-signature-style': 'off',
     'unicorn/prefer-string-slice': 'off',
     'unicorn/no-array-reduce': 'off',
@@ -76,12 +79,9 @@ module.exports = {
     'comma-dangle': [2, 'always-multiline'],
     'no-undef': 'off',
     'unicorn/no-array-for-each': 'off',
+    'unicorn/no-await-expression-member': 'off',
     'multiline-ternary': 'off',
     'security/detect-object-injection': 'off',
-    'unicorn/prefer-dom-node-append': 'off',
-    'unicorn/prefer-modern-dom-apis': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'unicorn/prefer-add-event-listener': 'off',
     'security/detect-non-literal-fs-filename': 'off',
     'unicorn/filename-case': [
       0,
@@ -93,27 +93,33 @@ module.exports = {
     'unicorn/consistent-function-scoping': [0],
     'no-void': [0],
     'unicorn/prefer-ternary': 'off',
+    'no-use-before-define': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
     semi: [0],
-    '@typescript-eslint/no-use-before-define': [1],
-    '@typescript-eslint/no-unused-vars': [
-      'warn', // or error
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': ['warn', { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }],
+    'dprint-integration/dprint': [
+      'warn',
+      // Global Config (will pass to the dprint formatter directly): Available at https://dprint.dev/config/
       {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_',
+        useDprintJson: true,
+      },
+      // Plugin Specific Config (will pass to the dprint plugins): Available at https://dprint.dev/plugins/
+      {
+        useDprintJson: true,
       },
     ],
+    // conflict with dprint-integration/dprint, will make some line disappear, and some line duplicated
+    '@typescript-eslint/consistent-type-imports': 'off',
+    'comma-dangle': 'off'
   },
   extends: [
     'eslint:recommended',
     'standard',
     'plugin:security/recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:unicorn/recommended',
-    'plugin:prettier/recommended',
     'standard-with-typescript',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
@@ -122,13 +128,14 @@ module.exports = {
     'plugin:import/warnings',
     'plugin:import/typescript',
     'plugin:react-hooks/recommended',
-    'prettier',
+    'plugin:dprint-integration/recommended',
+    'plugin:dprint-integration/disable-conflict',
     'plugin:security-node/recommended',
     'plugin:typescript-sort-keys/recommended',
   ],
   plugins: [
     '@typescript-eslint/eslint-plugin',
-    'prettier',
+    'dprint-integration',
     'react',
     'html',
     'typescript-sort-keys',
