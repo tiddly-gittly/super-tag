fd292c43ef251ffaf81d58d4e3564acfc968a2a6
lin onetwo <linonetwo012@gmail.com>
2024-05-11 01:43:23 +0800

    feat: allow translate enum title

--- src/super-tag/widgets/utils/translateSchema.ts
+++ src/super-tag/widgets/utils/translateSchema.ts
@@ -1,24 +1,30 @@
 import type { JSONSchema4 } from 'json-schema';
 
-const ignoredMetadataKeys = ['type', 'format', 'enum', 'propertyOrder'];
+const ignoredMetadataKeys = ['type', 'format', 'enum', 'propertyOrder', 'lingo-base'];
 /**
  * Translate trait tag's schema that has `lingo-base` property on top level.
  * @param traitTagSchema Schema from a trait tag's schema field
- * @example `{    "type": "object",    "lingo-base": "$:/plugins/linonetwo/intention-tower-knowledge-graph/language/",    "properties": {        "calendar_tag": {            "type": "string",            "title": "TraitTag/DefaultTagWhenAdd/Caption",            "description": "TraitTag/DefaultTagWhenAdd/Description"        }    }}`
+ * @example `{    "type": "object",    "lingo-base": "$:/plugins/linonetwo/intention-tower-knowledge-graph/language/",    "properties": {        "calendar_tag": {            "type": "string",            "title": "TraitTag/DefaultTagWhenAdd/Caption",            "description": "TraitTag/DefaultTagWhenAdd/Description",   "options": {         "enum_titles": [           "TraitTag/DefaultTagWhenAdd/Enum/1",           "TraitTag/DefaultTagWhenAdd/Enum/2"         ]       }  }    }}`
  */
 export function translateSchema(traitTagSchema: JSONSchema4): JSONSchema4 {
   const lingoBase = traitTagSchema['lingo-base'];
   if (!lingoBase || typeof lingoBase !== 'string') return traitTagSchema;
   const languageCode = $tw.wiki.filterTiddlers('[[$:/language]get[text]get[name]else[en-GB]]')[0]
-  const translatedProperties = Object.entries(traitTagSchema.properties ?? {}).reduce((acc, [key, fieldDefinition]) => {
+  const translatedProperties = Object.entries(traitTagSchema.properties ?? {}).reduce((acc, [propertyKey, propertyDefinition]) => {
     // translate fields like `title` `description` in `fieldDefinition`. But ignore fields like `type` `format` `enum` `propertyOrder`.
-    if (typeof fieldDefinition !== 'object' || !fieldDefinition) return acc;
-    const translatedFieldDefinition = Object.entries(fieldDefinition).reduce((acc, [fieldKey, fieldValue]) => {
-      if (ignoredMetadataKeys.includes(fieldKey) || typeof fieldValue !== 'string') return { ...acc, [fieldKey]: fieldValue };
-      const translatedFieldValue = $tw.wiki.getTiddlerText(`${lingoBase}${languageCode}/${fieldValue}`, fieldValue);
-      return { ...acc, [fieldKey]: translatedFieldValue };
+    if (typeof propertyDefinition !== 'object' || !propertyDefinition) return acc;
+    const translatedFieldDefinition = Object.entries(propertyDefinition).reduce((acc, [propertyDefinitionField, propertyDefinitionValue]) => {
+      if (propertyDefinitionField === 'options' && typeof propertyDefinitionValue === 'object' && propertyDefinitionValue) {
+        // translate enum_titles
+        const translatedEnumTitles = (propertyDefinitionValue as Record<string, string[]>).enum_titles?.map((enumTitle) => $tw.wiki.getTiddlerText(`${lingoBase}${languageCode}/${enumTitle}`, enumTitle));
+        return { ...acc, options: { ...propertyDefinitionValue, enum_titles: translatedEnumTitles } };
+      }
+      if (ignoredMetadataKeys.includes(propertyDefinitionField) || typeof propertyDefinitionValue !== 'string') return { ...acc, [propertyDefinitionField]: propertyDefinitionValue };
+      // translate other fields
+      const translatedFieldValue = $tw.wiki.getTiddlerText(`${lingoBase}${languageCode}/${propertyDefinitionValue}`, propertyDefinitionValue);
+      return { ...acc, [propertyDefinitionField]: translatedFieldValue };
     }, {} as Record<string, string>);
-    acc[key] = translatedFieldDefinition;
+    acc[propertyKey] = translatedFieldDefinition;
     return acc;
   }, {} as Record<string, JSONSchema4>);
   return {
